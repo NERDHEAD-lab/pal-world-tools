c69e62e9aafa967d25ff78bf7dbdd3a35ccbc895
류주성 <nerdlab.kr@gmail.com>
2024-02-18 19:11:12 +0900

    feat: setting submodules

---
+++ pal-world-tools-core/src/main/java/kr/nerdlab/palworld/PalWorldToolsApplication.java
@@ -0,0 +1,13 @@
+package kr.nerdlab.palworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalWorldToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalWorldToolsApplication.class, args);
+	}
+
+}
---
+++ pal-world-tools-core/src/test/java/kr/nerdlab/palworld/PalWorldToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.nerdlab.palworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalWorldToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
